016a1e7968f3683de49464885949f1687d8d4ede
hasanrc24 <hasanrc24@gmail.com>
2022-11-15 00:06:35 +0600

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components'
+import Nav from './Nav';
+
+const Header = () => {
+  return (
+    <HeaderSection>
+        <NavLink to="/">
+            <img src='./images/logo.png' height="60rem" alt='logo' />
+        </NavLink>
+        <Nav />
+    </HeaderSection>
+  )
+}
+
+const HeaderSection = styled.header`
+    padding: 0 4.8rem;
+    height: 5rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    .logo {
+    height: 5rem;
+    }
+`;
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,207 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { FiShoppingCart } from "react-icons/fi";
+import { CgMenu, CgClose } from "react-icons/cg";
+
+const Nav = () => {
+
+    const [menuIcon, setMenuIcon] = useState();
+
+const Nav = styled.nav`
+    .navbar-lists {
+        display: flex;
+        gap: 1.5rem;
+        align-items: center;
+        list-style: none;
+        .navbar-link {
+        &:link,
+        &:visited {
+            display: inline-block;
+            text-decoration: none;
+            font-size: 0.9rem;
+            text-transform: uppercase;
+            color: ${({ theme }) => theme.colors.black};
+            transition: color 0.3s linear;
+        }
+        &:hover,
+        &:active {
+            color: ${({ theme }) => theme.colors.helper};
+        }
+        }
+    }
+    .mobile-navbar-btn {
+        display: none;
+        background-color: transparent;
+        cursor: pointer;
+        border: none;
+    }
+    .mobile-nav-icon[name="close-outline"] {
+        display: none;
+    }
+    .close-outline {
+        display: none;
+    }
+    .cart-trolley--link {
+        position: relative;
+        .cart-trolley {
+        position: relative;
+        font-size: 1rem;
+        }
+        .cart-total--item {
+        width: 1rem;
+        height: 1rem;
+        font-size: 0.7rem;
+        position: absolute;
+        background-color: #000;
+        color: #000;
+        border-radius: 50%;
+        display: grid;
+        place-items: center;
+        top: -20%;
+        left: 70%;
+        background-color: ${({ theme }) => theme.colors.helper};
+        }
+    }
+    .user-login--name {
+        text-transform: capitalize;
+    }
+    .user-logout,
+    .user-login {
+        font-size: 1rem;
+        padding: 0.8rem 1.4rem;
+    }
+    @media (max-width: ${({ theme }) => theme.media.mobile}) {
+        .mobile-navbar-btn {
+        display: inline-block;
+        z-index: 9999;
+        border: ${({ theme }) => theme.colors.black};
+        .mobile-nav-icon {
+            font-size: 2rem;
+            color: ${({ theme }) => theme.colors.black};
+        }
+        }
+        .active .mobile-nav-icon {
+        display: none;
+        font-size: 2rem;
+        position: absolute;
+        top: 30%;
+        right: 10%;
+        color: ${({ theme }) => theme.colors.black};
+        z-index: 9999;
+        }
+        .active .close-outline {
+        display: inline-block;
+        }
+        .navbar-lists {
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        visibility: hidden;
+        opacity: 0;
+        transform: translateX(100%);
+        /* transform-origin: top; */
+        // transition: all 1s linear;
+        }
+        .active .navbar-lists {
+        visibility: visible;
+        opacity: 1;
+        transform: translateX(0);
+        z-index: 999;
+        transform-origin: right;
+        // transition: all 1s linear;
+        .navbar-link {
+            font-size: 1rem;
+        }
+        }
+        .cart-trolley--link {
+        position: relative;
+        .cart-trolley {
+            position: relative;
+            font-size: 1.3rem;
+        }
+        .cart-total--item {
+            width: 1.1rem;
+            height: 1.1rem;
+            font-size: 0.8rem;
+        }
+        }
+        .user-logout,
+        .user-login {
+        font-size: 2.2rem;
+        padding: 0.8rem 1.4rem;
+        }
+    }
+`;
+  return (
+    <Nav>
+        <div className={menuIcon ? "navbar active" : "navbar"}>
+        <ul className="navbar-lists">
+          <li>
+            <NavLink
+              to="/"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}>
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}>
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" className="navbar-link cart-trolley--link">
+              <FiShoppingCart className="cart-trolley" />
+              <span className="cart-total--item"> 10 </span>
+            </NavLink>
+          </li>
+        </ul>
+
+        {/* two button for open and close of menu */}
+        <div className="mobile-navbar-btn">
+          <CgMenu
+            name="menu-outline"
+            className="mobile-nav-icon"
+            onClick={() => setMenuIcon(true)}
+          />
+          <CgClose
+            name="close-outline"
+            className="mobile-nav-icon close-outline"
+            onClick={() => setMenuIcon(false)}
+          />
+        </div>
+      </div>
+    </Nav>
+  )
+}
+
+
+
+export default Nav;
